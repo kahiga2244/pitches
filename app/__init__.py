adc6310c52e4b5c997f9cbd70d258e55c3f5e5eb
kahiga2244 <kahigakamiru@gmail.com>
2021-03-01 14:11:52 +0300

    add twitter bootstrap in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__)
 
 #setting up configuration
 app.config.from_object(DevConfig)
+
+#Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
 from app import error
