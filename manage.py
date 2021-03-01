c06c20a41b9910f2e1a32b11a94b208ad87684f7
kahiga2244 <kahigakamiru@gmail.com>
2021-03-01 15:41:34 +0300

    trying to update

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from app.models import User
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
 
 # Creating app instance
@@ -9,5 +10,8 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
 if __name__ == '__main__':
-    manager.run(debug = True)
+    manager.run()
